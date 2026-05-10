6225588fdd3f360d625e6943bf2b35c1b70db5f6
blanncahr-crypto <blanncahr@gmail.com>
2026-05-10 14:50:06 -0600

    Integración Firebase con Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MiCineApp());
 }
 
@@ -198,7 +206,7 @@ class _InicioScreenState extends State<InicioScreen> {
               ),
               title: Text(p['name']),
             );
-          }).toList(),
+          }),
         ],
       ),
     );
